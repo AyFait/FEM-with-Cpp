cc27b40edcd91bfce48318b9c2ee13a4ff516c3d
Ayobami Faith Jr. <50885913+AyFait@users.noreply.github.com>
2026-02-17 13:23:14 +0100

    Update main for.cc

--- main for.cc
+++ main for.cc
@@ -1,1 +1,12 @@
+#include <iostream> // Include the iostream library for input/output operations
 
+
+// The following code demonstrates the use of "for" loops to iterate over a range of numbers and output them to the console. for(declaration; condition; increment/decrement) { // loop body }
+
+int main() {
+        for(int i=0; i<5; i++) { // Loop from 0 to 4, incrementing 'i' by 1 in each iteration (i++ is same as i = i + 1, i += 1)
+            std::cout << i <<" "; // Output the current value of 'i' followed by a space
+            std::cout << i << std::endl; // Output the current value of 'i' followed by a newline
+        }
+        return 0;
+    }
