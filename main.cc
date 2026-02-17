bf5aec0e9a0c6cb70283a6c5a65bad8b0e5897cb
AyFait <sonibareayfate@gmail.com>
2026-02-17 11:28:58 +0100

    first function

---
+++ main.cc
@@ -0,0 +1,7 @@
+#include <iostream> // Include the iostream library for input/output operations
+
+// The main function (an integer) is the entry point of the program
+int main(){             
+    std::cout << "Hello, World!" << std::endl; // Output "Hello, World!" to the console followed by a newline
+    return 0;
+}
