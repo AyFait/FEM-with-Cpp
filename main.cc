44cae2be47b572b685d4c291eed7312ff3ca6798
AyFait <sonibareayfate@gmail.com>
2026-02-17 11:51:02 +0100

    update main.cc

--- main.cc
+++ main.cc
@@ -5,3 +5,23 @@ int main(){
     std::cout << "Hello, World!" << std::endl; // Output "Hello, World!" to the console followed by a newline
     return 0;
 }
+
+//Basic number types in C++ include:
+//1. Integer types: int, short, long, long long 
+//2. Unsigned int: Non-negative integer types
+//3. Double: real numbers with double precision (64-bit)
+//4. Floating-point types: float, double, long double (like double but 32-bit precision)
+
+
+int main2() {
+        int a;
+        int b, c;
+        int d = 1, e = 3, f;
+
+        float g = 3.14f; // 'f' suffix indicates a float literal
+        double h = 2.71828; // double precision floating-point literal
+        
+        return 0;
+
+
+}
